80ee3972ce1184102d5a7485f92e03462cf49b6c
White Tiger <dayley.zimmermann@gmail.com>
2022-04-26 11:20:21 -0400

    lesson 2

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,14 +1,13 @@
 import React, { useEffect } from 'react';
-import { useStoreContext } from '../../utils/GlobalState';
-import { UPDATE_PRODUCTS } from '../../utils/actions';
 import { useQuery } from '@apollo/client';
 
 import ProductItem from '../ProductItem';
+import { useStoreContext } from '../../utils/GlobalState';
+import { UPDATE_PRODUCTS } from '../../utils/actions';
 import { QUERY_PRODUCTS } from '../../utils/queries';
 import spinner from '../../assets/spinner.gif';
 
 function ProductList() {
-
   const [state, dispatch] = useStoreContext();
 
   const { currentCategory } = state;
@@ -19,7 +18,7 @@ function ProductList() {
     if (data) {
       dispatch({
         type: UPDATE_PRODUCTS,
-        products: data.products
+        products: data.products,
       });
     }
   }, [data, dispatch]);
@@ -29,13 +28,15 @@ function ProductList() {
       return state.products;
     }
 
-    return state.products.filter(product => product.category._id === currentCategory);
+    return state.products.filter(
+      (product) => product.category._id === currentCategory
+    );
   }
 
   return (
     <div className="my-2">
       <h2>Our Products:</h2>
-      {products.length ? (
+      {state.products.length ? (
         <div className="flex-row">
           {filterProducts().map((product) => (
             <ProductItem
